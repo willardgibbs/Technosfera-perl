823333145d37d4d8e53ee3ed1ade728a8131bd0e
willardgibbs <kzavertkin@mail.ru>
2017-04-17 18:52:26 +0300

    without paral

--- homeworks/crawler/lib/Crawler.pm
+++ homeworks/crawler/lib/Crawler.pm
@@ -36,18 +36,28 @@ $total_size - суммарный размер собранных ссылок в
 @top10_list - top-10 страниц отсортированный по размеру.
 
 =cut
-my $hash = {};
-sub run {
-    my ($start_page, $parallel_factor) = @_;
-    $start_page or die "You must setup url parameter";
-    $parallel_factor or die "You must setup parallel factor > 0";
-    my $total_size = 0;
-    my @top10_list;
+my $result = {};
+
+sub top_10 {
+	my $result = shift;
+	my $total_size;
+	my @top10_list;
+	for (keys %$result) {
+		$total_size += $result->{$_};
+	}
+	my @mass = sort { $result->{$b} <=> $result->{$a} } keys %$result;
+	@top10_list = @mass[0..9];
+	return $total_size, @top10_list;
+}
+
+sub myrequests {
+	my $start_page = shift;
+	my $ref_on_hrefs;
     STDOUT->autoflush;
 	my $exit_wait = AnyEvent->condvar;
 	my $exit_wait1 = AnyEvent->condvar;
-	open(my $fh, ">", "logs.txt");
 	my $flag;
+
 	my $handle = http_request
 		HEAD => $start_page,
 		timeout => 1,
@@ -55,62 +65,43 @@ sub run {
 			my ($body, $hdr) = @_;
 			if ($hdr->{Status} == 200) {
 				$flag = $hdr->{"content-type"} =~ /text\/html/;
-			} else {
-				print {$fh} "Fail: @$hdr{qw(Status Reason)}";
 			}
 			$exit_wait->send;
 		};
 	$exit_wait->recv;
 	return unless $flag;
-	my @lol;
+
 	my $handle1 = http_request
 		GET => $start_page,
 		timeout => 1,
 		sub {
 			my ($body, $hdr) = @_;
 			if ($hdr->{Status} == 200) {
-				$hash->{$start_page} = length(Encode::encode_utf8($body));
-				@lol = $body =~ /href="([^#"][^"]+)"/g;
-			} else {
-				print {$fh} "Fail: @$hdr{qw(Status Reason)}";
+				$result->{$start_page} = length(Encode::encode_utf8($body));
+				@$ref_on_hrefs = $body =~ /href="(\/[^#"][^"]+)"/g;
 			}
 			$exit_wait1->send;
 		};
 	$exit_wait1->recv;
-	$start_page =~ /(http\w?:\/\/[^\/]+\/)/;
-	my $str = $1;
-	my $base = URI->new($str);
-	my @kek = ();
-	push @kek, URI->new_abs($_, $base) for (@lol);
-	print {$fh} join "\n", @kek;
-
-	for my $i (@kek) {
-		unless (exists($hash->{$i}) or keys %$hash == 1000) {
-			run($i, 1);
-			p $hash;
-		}
-	}
-
-# #параллельное выполнение с лимитом 3
-# 	my $cv = AE::cv;
-# 	my @array = 1..10;
-# 	my $i = 0;
-# 	my $next; 
-# 	$next = sub {
-# 		my $cur = $i++;
-# 		return if $cur > $#array;
-# 		say "Process $array[$cur]";
-# 		async sub {
-# 			say "Processed $array[$cur]";
-# 			$next->();
-# 		};
-# 	}; $next->() for 1..3;
-# 	$cv->recv;
-
-# #AE:HTTP
-
-
-#     return $total_size, @top10_list;
+	return $ref_on_hrefs;
 }
-run("https://github.com/Nikolo/Technosfera-perl/tree/anosov-crawler/", 1);
+
+sub run {
+    my ($start_page, $parallel_factor) = @_;
+    $start_page =~ /http\w?:\/\/[^\/]+(\/.+)/;
+	my $url = $1;
+	my $href_inside = [];
+    my $ref_on_hrefs = myrequests($start_page);
+	for (@$ref_on_hrefs) {
+		push @$href_inside, $start_page.$url if ($_ =~ /^$url(.+)$/);
+	};
+	for my $var (@$href_inside) {
+		run($var, 1) unless exists($result->{$var}) or keys %$result == 1000;
+	}
+    return top_10($result);
+}
+my ($total_size, @top10_list) = run("https://github.com/Nikolo/Technosfera-perl/tree/anosov-crawler", 1);
+my $tmp = int($total_size/1024);
+p $tmp;
+p @top10_list;
 1;
