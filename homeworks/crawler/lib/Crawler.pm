95030f8db9739d7a88d2db15f5923b9e262c5b7d
willardgibbs <kzavertkin@mail.ru>
2017-05-08 14:49:58 +0300

    all test have passed

--- homeworks/crawler/lib/Crawler.pm
+++ homeworks/crawler/lib/Crawler.pm
@@ -4,12 +4,12 @@ use 5.010;
 use strict;
 use warnings;
 
-use AnyEvent;
 use AnyEvent::HTTP;
 use Web::Query;
 use URI;
+use HTML::LinkExtor;
 use DDP;
-use Encode;
+$AnyEvent::HTTP::MAX_PER_HOST = 100;
 =encoding UTF8
 
 =head1 NAME
@@ -27,107 +27,68 @@ Web Crawler
 Входные данные:
 
 $start_page - Ссылка с которой надо начать обход сайта
-
 $parallel_factor - Значение фактора паралельности
 
 Выходные данные:
 
 $total_size - суммарный размер собранных ссылок в байтах
-
 @top10_list - top-10 страниц отсортированный по размеру.
 
 =cut
-my $result = {};
-
-sub top_10 {
-	my $result = shift;
-	my $total_size;
-	my @top10_list;
-	for (keys %$result) {
-		$total_size += $result->{$_};
-	}
-	my @mass = sort { $result->{$b} <=> $result->{$a} } keys %$result;
-	@top10_list = @mass[0..9];
-	return $total_size, @top10_list;
-}
-
-my $get = sub {
-	my $start_page = shift;
-	my $ref_on_hrefs = [];
-	my $handle1 = http_request
-		GET => $start_page,
-		timeout => 1,
-		sub {
-			say "lol";
-			my ($body, $hdr) = @_;
-			if ($hdr->{Status} == 200) {
-				$result->{$start_page} = length(Encode::encode_utf8($body));
-				@$ref_on_hrefs = $body =~ /href="(\/[^#"][^"]+)"/g;
-			}
-		};
-	p $ref_on_hrefs;
-	return $ref_on_hrefs;
-};
-
-sub head {
-	my $cb = shift;
-	my $start_page = shift;
-	my $flag;
-	my $top_kek;
-	http_request
-		HEAD => $start_page,
-		timeout => 1,
-		$top_kek = sub {
-			my ($body, $hdr) = @_;
-			if ($hdr->{Status} == 200) {
-				if ($flag = $hdr->{"content-type"} =~ /text\/html/) {
-					undef $top_kek;
-					return $cb->($start_page);
-				} else {
-					return;
-				}
-			}
-		};
-}
 
 sub run {
-    my ($start_page, $parallel_factor) = @_;
-	my $href_inside = [];
-    my $ref_on_hrefs = [];
-
-    #STDOUT->autoflush;
-
-	my $cv = AE::cv;  
+	my ($start_page, $parallel_factor) = @_;
+	my $total_size;	
+	my @top10_list;
+	my $limit = 1000;
+	my %unic;
+	$start_page =~ /http\w?:\/\/[^\/]+(\/.+)/;
+	my $part = $1;
+	my @links = ($start_page);
+	my $num = 0;
+	my $cv = AnyEvent->condvar;
 	$cv->begin;
-	my $next;
-	$next = sub {
-		$start_page =~ /http\w?:\/\/[^\/]+(\/.+)/;
-		my $url = $1;
+	my $next; $next = sub {
+		$num++;
+		unless ((keys(%unic) < $limit) && (@links)) {
+			$cv->send;
+			return;
+		}
+		my $link = shift @links;
 		$cv->begin;
-		$ref_on_hrefs = head $start_page, sub {
-			$next->();
-			$cv->end;
-		};
-		p $ref_on_hrefs;
-		# = get($start_page) if head($start_page);
+		http_head
+			$link,
+			sub {
+				my ($body, $header) = @_;
+				if ($header->{"content-type"} =~ "text/html") {	
+					$cv->begin;
+					http_get
+						$link,
+						sub {
+							$body = shift;
+							$unic{$link} = length $body;
+							my @href = $body =~ /href="(\/[^#"][^"]+)"/g;
+							for (@href) {
+								my $tmp = $_ =~ /^$part(.+)$/;
+								push @links, $start_page.$1 if $tmp && !$unic{$start_page.$1};
+							}
+							my $min = $parallel_factor;
+							$min = @links if @links < $parallel_factor;
+							$next->() while ($num <= $min);
+							$num--;
+							$cv->end;
+							return;
+						};
+				}
+				$cv->end;
+			};
 
-		# for (@$ref_on_hrefs) {
-		# 	push @$href_inside, $start_page.$1 if ($_ =~ /^$url(.+)$/);
-		# }
-		# for my $var (@$href_inside) {
-		# 	return if exists($result->{$var}) or keys %$result == 1000;
-		# }
-
-	}; 
-	$next->() for 1..2;
-	$cv->end; 
+	}; $next->();
+	$cv->end;
 	$cv->recv;
-    return top_10($result);
+	@top10_list = sort {$unic{$b} <=> $unic{$a}} keys %unic;
+	$total_size += $unic{$_} for @top10_list;
+	@top10_list = @top10_list[0..9];
+	return $total_size, @top10_list;
 }
-
-my ($total_size, @top10_list) = run("https://github.com/Nikolo/Technosfera-perl/tree/anosov-crawler", 1);
-my $tmp = int($total_size/1024);
-p $tmp;
-p @top10_list;
-
 1;
